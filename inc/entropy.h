5a539ff1831325f00753b5479e18515cafb76619
neoflame99 <43754377+neoflame99@users.noreply.github.com>
2026-09-03 19:27:16 +0900

    add defines change getRowCmpSize
    Added new functions for group entropy encoding and decoding.

--- inc/entropy.h
+++ inc/entropy.h
@@ -4,20 +4,28 @@
 #define _ENTROPY_H_
 
 #define NSGRP 8
+#define SZSZ  2  // byte size of a row bitstream
+#define KBSZ  4  // bit size of K to store
 using namespace std;
 
 inline uint32_t getRowCmpSize(vector<uint8_t>& rRow){
-    uint32_t rsza[4];
-    for(int k=0; k < 4; ++k){
+    uint32_t rsza[SZSZ];
+    for(int k=0; k < SZSZ; ++k){
         rsza[k] = rRow[k];
+    }    
+    uint32_t rsz = 0;
+    for(int k=0; k < SZSZ; ++k){
+         rsz |= rsza[k]<<(8*k);
     }
-    uint32_t rsz = rsza[0] | (rsza[1]<<8)|(rsza[2]<<16)|(rsza[3]<<24);
-    rsz -= 4;
+    //rsz -= 2;
     return rsz;
 }
 void enc_entropy(vector<vector<uint8_t>>& bitv, vector<msstSm>& msstv, const MsstInfo& msst_info, 
                  const ProcessInfo& proc_info, const QuantInfo& quant_info );
 void dec_entropy(vector<vector<uint8_t>>& bitv, vector<msstSm>& msstv, const MsstInfo& msst_info,
                  const ProcessInfo& proc_info, const QuantInfo& quant_info );
-
+void enc_entropy_gr(vector<vector<uint8_t>>& bitv, vector<msstSm>& msstv, const MsstInfo& msst_info,
+    const ProcessInfo& proc_info, const QuantInfo& quant_info );
+void dec_entropy_gr(vector<vector<uint8_t>>& bitv, vector<msstSm>& msstv, const MsstInfo& msst_info,
+    const ProcessInfo& proc_info, const QuantInfo& quant_info );
 #endif
